ca2e9a3f681dfdd93c2e3d3e2b64fe8de944780c
7u4bgr <abdllyevv@gmail.com>
2024-04-07 02:00:37 +0400

    remove padding

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,25 @@
-'use client'
+"use client";
 import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [input, setInput] = useState(""); // Kullanıcının girişlerini saklamak için
-  const [result, setResult] = useState(0); // Hesaplama sonucunu saklamak için
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(0);
 
   const handleClick = (value) => {
-    // Kullanıcının girişini güncelle
-    setInput(prevInput => prevInput + value);
+    setInput((prevInput) => prevInput + value);
   };
 
   const handleEqualsClick = () => {
     try {
-      // Kullanıcının girdiği ifadeyi işle ve sonucu sakla
-      // Eğer girişte "%" varsa, giriş değerini yüzdeye çevir
       const expression = input.includes("%") ? parseFloat(input) / 100 : input;
       setResult(eval(expression));
     } catch (error) {
-      // Hata varsa sonucu temizle
       setResult("Hata");
     }
   };
 
   const handleClearClick = () => {
-    // Girişi ve sonucu temizle
     setInput("");
     setResult("");
   };
@@ -40,30 +35,57 @@ export default function Home() {
           <button onClick={() => handleClearClick()}>AC</button>
           <button onClick={() => handleClick("%")}>%</button>
           <button onClick={() => handleClick("*")}>*</button>
-          <button className={styles.yellowbutton} onClick={() => handleClick("/")}>/</button>
+          <button
+            className={styles.yellowbutton}
+            onClick={() => handleClick("/")}
+          >
+            /
+          </button>
         </div>
         <div className={styles.list2}>
           <button onClick={() => handleClick(7)}>7</button>
           <button onClick={() => handleClick(8)}>8</button>
           <button onClick={() => handleClick(9)}>9</button>
-          <button onClick={() => handleClick("*")} className={styles.yellowbutton}>X</button>
+          <button
+            onClick={() => handleClick("*")}
+            className={styles.yellowbutton}
+          >
+            X
+          </button>
         </div>
         <div className={styles.list2}>
           <button onClick={() => handleClick(4)}>4</button>
           <button onClick={() => handleClick(5)}>5</button>
           <button onClick={() => handleClick(6)}>6</button>
-          <button onClick={() => handleClick("-")} className={styles.yellowbutton}>-</button>
+          <button
+            onClick={() => handleClick("-")}
+            className={styles.yellowbutton}
+          >
+            -
+          </button>
         </div>
         <div className={styles.list2}>
           <button onClick={() => handleClick(1)}>1</button>
           <button onClick={() => handleClick(2)}>2</button>
           <button onClick={() => handleClick(3)}>3</button>
-          <button onClick={() => handleClick("+")} className={styles.yellowbutton}>+</button>
+          <button
+            onClick={() => handleClick("+")}
+            className={styles.yellowbutton}
+          >
+            +
+          </button>
         </div>
         <div className={styles.list3}>
-          <button className={styles.zerobutton} onClick={() => handleClick(0)}>0</button>
+          <button className={styles.zerobutton} onClick={() => handleClick(0)}>
+            0
+          </button>
           <button onClick={() => handleClick(".")}>.</button>
-          <button onClick={() => handleEqualsClick()} className={styles.yellowbutton}>=</button>
+          <button
+            onClick={() => handleEqualsClick()}
+            className={styles.yellowbutton}
+          >
+            =
+          </button>
         </div>
       </div>
     </main>
